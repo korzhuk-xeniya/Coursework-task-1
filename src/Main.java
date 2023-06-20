580e916f6a940f1f466a23dbfa09c8e773b762ba
Ксения <korzhuk.xeniya@yandex.ru>
2023-06-20 22:56:12 +0300

    done

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Main {
+    public static String reverseArray(String[] word) {
+        String[] reverceWord = new String[word.length];
+        int reverceLetter = 0;
+        for (int letters = word.length - 1; letters >= 0; letters--) {
+            reverceWord[reverceLetter] = word[letters];
+            reverceLetter++;
+        }
+        return Arrays.toString(reverceWord);
+    }
+
+    public static void main(String[] args) {
+        String[] word = {"т", "о", "п", "о", "р"};
+        System.out.println(reverseArray(word));
+
+    }
+}
